83f021176a487be190820aa4d4fd8b9641b865b1
CristianGC006 <cogolloguzmanesteban@gmail.com>
2024-11-14 16:04:09 -0500

    Avances en el login con alertas de mensaje y enlace a ViewCredito

--- helpers/expresiones.js
+++ helpers/expresiones.js
@@ -1,7 +1,7 @@
 export const expresionesRegulares = {
     nombre: /^[A-ZÁÉÍÓÚÜÑ\s]+$/, // Solo letras en mayúsculas, incluyendo acentos y espacios.  
     usuario: /^[a-z]{4,10}$/, // Solo letras minúsculas, entre 4 y 10 caracteres.  
-    contraseña: /^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{4,10}$/, 
+    contraseña: /^(?=.{8,})(?=.\d)(?=.[a-z])(?=.[A-Z])(?!.\s).*$/gm, 
     // Entre 4 y 10 caracteres, con al menos una mayúscula, una minúscula, un número y un carácter especial.  
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ // Formato de correo electrónico 
 };
