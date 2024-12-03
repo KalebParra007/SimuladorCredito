5d68f0f6c949b60ede4d328f9a8c14f09ec11848
Esteban <ezapata12@cesde.net>
2024-12-03 16:29:55 -0500

    Se avanzo más

--- controllers/ControllerUsuario.js
+++ controllers/ControllerUsuario.js
@@ -1,61 +1,80 @@
-import { usuarios } from "../models/ModelUsuario.js";
+let urlUsuarios = "http://localhost:3000/modelUsuario";
+let usuarios = [];
 
-export function login(){
-    let loginUsuario = document.getElementById('loginUsuario').value
-    let loginContraseña = document.getElementById('loginContraseña').value
-    console.log(usuarios);
-    usuarios.some(function(index){
-        if(loginUsuario == index.usuario && loginContraseña == index.contraseña){
-            let timerInterval;
-Swal.fire({
-  title: "Bienvenido: " + index.nombre,
-  html: "I will close in <b></b> milliseconds.",
-  timer: 2000,
-  icon: "success",
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading();
-    const timer = Swal.getPopup().querySelector("b");
-    timerInterval = setInterval(() => {
-      timer.textContent = `${Swal.getTimerLeft()}`;
-    }, 100);
-  },
-  willClose: () => {
-    clearInterval(timerInterval);
-     window.location.href = '/views/pages/ViewCredito.html'
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log("I was closed by the timer");
-  }
-});
-
-        } else{
-            Swal.fire({
-                title: "Error",
-                text: "Usuario y/o contraseña incorrecto o no existente",
-                icon: "error"
-              });
-        }
-    });
+function getUsuario() {
+  fetch(urlUsuarios)
+    .then((response) => response.json)
+    .then((data) => {
+      usuarios = data;
+    })
+    .catch((error) => console.log(error));
 }
 
-export function register(){
-    let registroNombre = document.getElementById('registerNombre').value;
-    let registroUsuario = document.getElementById('registerUsuario').value;
-    let registroCorreo = document.getElementById('registerCorreo').value;
-    let registroContraseña = document.getElementById('registerContraseña').value;
-    let registerConfirmarContr = document.getElementById('registerConfirmarContr').value;
-    let newRegistro = {
-        nombre: registroNombre,
-        usuario: registroUsuario,
-        correo: registroCorreo,
-        contraseña: registroContraseña,
-        confirmarContraseña: registerConfirmarContr
+getUsuario();
+
+export function login() {
+  let loginUsuario = document.getElementById("loginUsuario").value;
+  let loginContraseña = document.getElementById("loginContraseña").value;
+
+  usuarios.some(function (index) {
+    if (loginUsuario == index.usuario && loginContraseña == index.contraseña) {
+      let timerInterval;
+      Swal.fire({
+        title: "Bienvenido: " + index.nombre,
+        html: "I will close in <b></b> milliseconds.",
+        timer: 2000,
+        icon: "success",
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const timer = Swal.getPopup().querySelector("b");
+          timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+          window.location.href = "/views/pages/ViewCredito.html";
+        },
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
+        }
+      });
+    } else {
+      Swal.fire({
+        title: "Error",
+        text: "Usuario y/o contraseña incorrecto o no existente",
+        icon: "error",
+      });
     }
-    usuarios.push(newRegistro)
-    document.getElementById("form-login").style.display = 'flex'
-    document.getElementById("form-register").style.display = 'none'
- 
+  });
+}
+
+export function register() {
+  let registroNombre = document.getElementById("registerNombre").value;
+  let registroUsuario = document.getElementById("registerUsuario").value;
+  let registroCorreo = document.getElementById("registerCorreo").value;
+  let registroContraseña = document.getElementById("registerContraseña").value;
+  let registerConfirmarContr = document.getElementById(
+    "registerConfirmarContr"
+  ).value;
+  let newRegistro = {
+    nombre: registroNombre,
+    usuario: registroUsuario,
+    correo: registroCorreo,
+    contraseña: registroContraseña,
+    confirmarContraseña: registerConfirmarContr,
+  };
+  document.getElementById("form-login").style.display = "flex";
+  document.getElementById("form-register").style.display = "none";
+  agregarUsuario(newRegistro);
+}
+
+function agregarUsuario(newRegistro) {
+  fetch(urlUsuarios, {
+    method: "POST",
+    body: JSON.stringify(newRegistro),
+  });
 }
--- helpers/expresiones.js
+++ helpers/expresiones.js
@@ -1,7 +1,7 @@
 export const expresionesRegulares = {
     nombre: /^[A-ZÁÉÍÓÚÜÑ\s]+$/, // Solo letras en mayúsculas, incluyendo acentos y espacios.  
     usuario: /^[a-z]{4,10}$/, // Solo letras minúsculas, entre 4 y 10 caracteres.  
-    contraseña: /^(?=.{8,})(?=.\d)(?=.[a-z])(?=.[A-Z])(?!.\s).*$/gm, 
+    contraseña:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, 
     // Entre 4 y 10 caracteres, con al menos una mayúscula, una minúscula, un número y un carácter especial.  
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ // Formato de correo electrónico 
 };
